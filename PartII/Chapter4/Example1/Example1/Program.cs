af630cbce1744cfc89c6bc15fe8ce18aab282b37
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-11 11:50:54 +0000

    Cast Object example

--- PartII/Chapter4/Example1/Example1/Program.cs
+++ PartII/Chapter4/Example1/Example1/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Example1
 {
-    class Program
+    public sealed class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Object o = new Employee();
+
+            Employee e = (Employee) o;
+
+            Manager m = new Manager();
+
+            PromoteEmployee(m);
+
+            DateTime newYears = new DateTime(2013, 1, 1);
+            PromoteEmployee(newYears);
         }
+
+        private static void PromoteEmployee(Object o)
+        {
+            Employee e = (Employee) o;
+        }
+    }
+
+    internal class Employee
+    {
+        
+    }
+
+    internal class Manager : Employee
+    {
+        
     }
 }
