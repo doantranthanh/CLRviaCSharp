df62e13a140e94f90a64fadd329b5b2cdb81f24c
Thanh Doan <T.Doan@xlntelecom.co.uk>
2014-11-11 12:19:09 +0000

    Add comment

--- PartII/Chapter4/Example1/Example1/Program.cs
+++ PartII/Chapter4/Example1/Example1/Program.cs
@@ -10,16 +10,27 @@ namespace Example1
     {
         public static void Main(string[] args)
         {
+            //Cast example 1
+            //No cast needed
             Object o = new Employee();
-
             Employee e = (Employee) o;
 
+            //Cast example 2
+            /* this line of codes will work both runtime and compile time
+             * because m object has a IS - A relation, in fact it is a derived class from Employee class
+             */
             Manager m = new Manager();
-
             PromoteEmployee(m);
+            
+            /* this line of codes will cause a problem at runtime
+             * because newYears object is not Employee or a derived class from Employee
+             * at compile time, it works because the parameter for PromoteEmployee is an instance of object
+             * in other word, DateTime object is derived from Object
+             */
+            //DateTime newYears = new DateTime(2013, 1, 1);
+            //PromoteEmployee(newYears);
 
-            DateTime newYears = new DateTime(2013, 1, 1);
-            PromoteEmployee(newYears);
+            //Casting with the C# is and as Operators
         }
 
         private static void PromoteEmployee(Object o)
